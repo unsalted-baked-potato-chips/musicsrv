dfd18c99be31c4c4527a22de7b1efa5407619839
Robert Herlihy <29720977+BroccoliRaab@users.noreply.github.com>
2022-02-17 09:29:17 -0500

    Handle post buttons

--- musrv.go
+++ musrv.go
@@ -10,7 +10,21 @@ func searchForSong(s string){
 
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request){
+func handlePost(w *http.ResponseWriter, r *http.Request){
+    if err := r.ParseForm(); err != nil {
+        fmt.Fprintf(*w, "ParseForm() err: %v", err)
+		return
+	}
+    switch r.FormValue("playerControl") {
+        case "play":
+            fmt.Fprintf(*w, "Playing")
+        case "pause":
+            fmt.Fprintf(*w, "Pausing")
+        case "skip":
+            fmt.Fprintf(*w, "Skipping")
+        default "":
+        http.Error(*w, "Unsupported Player Control", http.StatusNotFound)
+    }
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request){
@@ -20,18 +34,18 @@ func requestHandler(w http.ResponseWriter, r *http.Request){
     }
 
     switch r.Method {
-    case "GET":
-        q := r.URL.Query().Get("search")
-        if q == ""{
-            http.ServeFile(w, r, "index.html")
-            return
-        }
-        fmt.Fprintf(w, q)
-        searchForSong(q)
-    case "POST":
-        handlePost(w, r)
-    default:
-        http.Error(w, "Method is not supported.", http.StatusNotFound)
+        case "GET":
+            q := r.URL.Query().Get("search")
+            if q == ""{
+                http.ServeFile(w, r, "index.html")
+                return
+            }
+            fmt.Fprintf(w, q)
+            searchForSong(q)
+        case "POST":
+            handlePost(&w, r)
+        default:
+            http.Error(w, "Method is not supported.", http.StatusNotFound)
     }
 }
 
@@ -40,7 +54,5 @@ func main () {
     fmt.Printf("Starting server for testing HTTP POST...\n")
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal(err)
-
     }
-
 }
