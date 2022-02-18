31ba8c7008911b25e8b41d05e7627b5a462d218c
robert <robert@bertsd.myfiosgateway.com>
2022-02-17 23:37:04 -0500

    download and play m4as

--- musrv.go
+++ musrv.go
@@ -10,6 +10,7 @@ import (
     "bytes"
     "html/template"
     "encoding/json"
+    "strings"
 )
 type YTID struct {
     Kind string `json:"kind"`
@@ -29,7 +30,7 @@ type YTResponse struct {
 var ytkey string
 
 func enqueueSong(s string) string{
-    cmd := exec.Command("youtube-dl","-x","--audio-format","m4a", s, "--get-filename")
+    cmd := exec.Command("youtube-dl","--restrict-filenames","-x","--audio-format","m4a", s, "--get-filename")
     var out bytes.Buffer
     cmd.Stdout = &out
 	err := cmd.Run()
@@ -39,14 +40,14 @@ func enqueueSong(s string) string{
         return ""
 	}
     fmt.Println("Downloading")
-    cmd = exec.Command("youtube-dl","-x","--audio-format","m4a", s)
+    cmd = exec.Command("youtube-dl","--restrict-filenames","-x","--audio-format","m4a", s)
 	err = cmd.Run()
 	if err != nil {
 		log.Println(err)
         return ""
 	}
     fmt.Println("Playing")
-    cmd = exec.Command("mpv",  out.String());
+    cmd = exec.Command("mpv",  strings.TrimSpace(out.String()));
 	err = cmd.Run()
 
 	if err != nil {
